aa9d8ad7e9f361860d93dbdedab33d08b44fb588
Raz Bikel <razb@Macbook-RazBikel.local>
2022-10-15 13:24:32 +0300

    add spaceX API to launches mongodb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,19 +1,13 @@
 
 const launchesDataBase = require('./launches.mongo');
 const planets = require('./launches.mongo');
+const axios = require('axios');
 
 const LATEST_FLIGHT_NUMBER = 100;
 
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
+
 const saveLaunch = async (launch) => {
-
-    const planet = await planets.findOne({
-        keplerName: launch.target
-    })
-
-    if(!planet){
-        throw new Error('no matching planet found!')
-    }
-
     await launchesDataBase.findOneAndUpdate({
         flightNumber: launch.flightNumber
     }, launch, {
@@ -45,8 +39,7 @@ const launch = {
 saveLaunch(launch)
 
 const existsLaunchWithId = async (launchId) => {
-    // return launches.has(launchId);
-    return await launchesDataBase.findOne({
+    return await findLaunch({
         flightNumber: launchId
     })
 }
@@ -59,6 +52,14 @@ const getAllLaunches = async () => {
 
 
 const scheduleNewLaunch = async (launch) => {
+    const planet = await planets.findOne({
+        keplerName: launch.target
+    })
+
+    if(!planet){
+        throw new Error('no matching planet found!')
+    }
+
     const newFlightNumber = await getLatestFlightNumber() + 1;
     const newLaunch = Object.assign(launch, {
         success: true,
@@ -83,9 +84,83 @@ const deleteNewLaunch = async (id) => {
 }
 
 
+const findLaunch = async (filter) => {
+    return await launchesDataBase.findOne(filter)
+}
+
+
+const populateLaunches = async () =>{
+    console.log('downloading launches data...')
+    const response = await axios.post(SPACEX_API_URL,{
+        query:{},
+        options:{
+            pagination: false,
+            populate:[
+                {
+                    path:"rocket",
+                    select:{
+                        name: 1
+                    }
+                },
+                {
+                    path:"payloads",
+                    select:{
+                        customers: 1
+                    }
+                }
+            ]
+        }
+    })
+
+    if(response.status !== 200){
+        console.log('problem downloading launch data')
+        throw new Error('Launch data downloading failed!')
+    }
+
+    const launch_docs = response.data.docs;
+    
+    launch_docs.forEach(async launch_doc => {
+        const payloads = launch_doc['payloads'];
+        const customers = payloads.flatMap(payload => {
+            return payload['customers'];
+        })
+
+        const launch = {
+            flightNumber: launch_doc['flight_number'],
+            mission: launch_doc['name'],
+            rocket: launch_doc['rocket']['name'],
+            launchDate: launch_doc['date_local'],
+            customers,
+            upcoming: launch_doc['upcoming'],
+            success: launch_doc['success']
+        }
+
+        await saveLaunch(launch)
+    
+    })
+}
+
+
+const loadLaunchesData = async () => {
+    const first_launch = await findLaunch({
+        flightNumber: 1,
+        rocket: 'Falcon 1',
+        mission: 'FalconSat'
+    })
+    if(first_launch){
+        console.log('launch data already loaded!')
+    }
+    else{
+        await populateLaunches();
+    }
+
+}
+
+
 module.exports = {
     getAllLaunches,
     deleteNewLaunch,
     existsLaunchWithId,
-    scheduleNewLaunch
+    scheduleNewLaunch,
+    loadLaunchesData
 }
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const app = require('../src/app');
 const http = require('http');
 const {load_planets} = require('../src/models/planets.model')
 const {mongoConnect} = require('./services/mongo')
+const{loadLaunchesData} = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,6 +12,7 @@ const run_server = async () => {
     console.log('process.env.NODE_ENV', process.env.NODE_ENV)
     await mongoConnect();
     await load_planets();
+    await loadLaunchesData();
     server.listen(PORT, () => {
         console.log(`listening to port ${PORT}...`)
     })
